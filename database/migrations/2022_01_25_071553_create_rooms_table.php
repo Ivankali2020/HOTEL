ddb75f1dc8328b6ca35a6af765c1b9c1ef3f9501
HeinKoKo <mgheinkokohero007@gmail.com>
2022-01-25 14:01:09 +0630

    first step

---
+++ database/migrations/2022_01_25_071553_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string("FeaturePhoto");
+            $table->string("RoomName");
+            $table->string("Description");
+            $table->string("Price");
+            $table->bigInteger("User_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
